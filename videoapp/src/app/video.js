04ddbd3c8ec99e081cb9a8d2fc58e5893b6498f9
saukeen <108343003+saukeen@users.noreply.github.com>
2024-04-23 06:49:09 +0000

    youtube video

--- videoapp/src/app/video.js
+++ videoapp/src/app/video.js
@@ -4,8 +4,9 @@ function Video() {
     return ( 
         <div>
             <h3>My earth video</h3>
-            <video src="https://file-examples.com/storage/fee868065066261f19c04c3/2017/04/file_example_MP4_480_1_5MG.mp4" controls width={200}>
-            </video>
+            <iframe width="250" src="https://www.youtube.com/embed/YrUwPR9DSuw?si=QmPlwkWC7_4qjQy6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+            </iframe>
+            
         </div>
      );
 }
