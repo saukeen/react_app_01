b3ee2638955997eb0a6ac2537a2be5c148bce769
saukeen <108343003+saukeen@users.noreply.github.com>
2024-04-23 06:02:43 +0000

    create root component

---
+++ videoapp/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyRootComponent from './root/rootcomponent';
+
+ReactDOM.render(<MyRootComponent></MyRootComponent>, document.getElementById("home"))
---
+++ videoapp/src/root/rootcomponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MyRootComponent() {
+    return (
+        <div>
+        <h1>Root component</h1>        
+        </div>
+      );
+}
+
+export default MyRootComponent;
